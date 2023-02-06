91b2a077475df8290a6937275945dcd0b115a6bc
Ing-Roger-Barrios <barriosroger87@gmailcom>
2023-02-06 14:57:08 -0400

    El sistema funciona

--- accesos/admin.py
+++ accesos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Acceso, Area, SecAcc, Seccion, ArSecc
+from .models import Acceso, Area, SecAcc, Seccion, ArSecc,persona, cargo, CarAcc
 # Register your models here.
 
+admin.site.site_header = "Sistema de Accesos Industrias Cabrera"
+admin.site.site_title = "Sistema de Accesos"
+admin.site.index_title = "Bienvenidos al portal de administración"
 
 @admin.register(Acceso)
 class AccesoAdmin(admin.ModelAdmin):
@@ -26,3 +29,16 @@ class SecAccInline(admin.TabularInline):
 class SeccionAdmin(admin.ModelAdmin):
     list_display = ('Nombre',)
     inlines = [SecAccInline]
+
+@admin.register(persona)
+class personaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','apellido','usr','cargo','fechaIngreso','fechaRetiro')
+
+class CarAccInline(admin.TabularInline):
+    model = CarAcc
+    extra = 1
+
+@admin.register(cargo)
+class cargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo',)
+    inlines = [CarAccInline]
--- accesos/models.py
+++ accesos/models.py
@@ -11,7 +11,8 @@ class Acceso(models.Model):
     def __str__(self):
         return self.Acceso
     class Meta:
-        verbose_name_plural = "--> Acceso"
+        verbose_name_plural = "3.- Acceso"
+
 
 class Seccion(models.Model):
     Nombre = models.CharField(max_length=200, verbose_name='Seccion',default='')
@@ -20,13 +21,13 @@ class Seccion(models.Model):
     def __str__(self):
         return self.Nombre
     class Meta:
-        verbose_name_plural = "--> Seccion"
+        verbose_name_plural = "2.- Seccion"
 
 class SecAcc(models.Model):
     Seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
     Acceso = models.ForeignKey(Acceso, on_delete=models.CASCADE)
     class Meta:
-        verbose_name_plural = "--> Secc_Acc"
+        verbose_name_plural = "Secc_Acc"
 
 class Area(models.Model):
     Area = models.CharField(max_length=200, verbose_name='Area',default='')
@@ -35,14 +36,39 @@ class Area(models.Model):
     def __str__(self):
         return self.Area
     class Meta:
-        verbose_name_plural = "--> Area"
+        verbose_name_plural = "1.- Area"
 
 class ArSecc(models.Model):
     Area = models.ForeignKey(Area, on_delete=models.CASCADE)
     Seccion = models.ForeignKey(Seccion, on_delete=models.CASCADE)
     class Meta:
-        verbose_name_plural = "--> ArSecc"
+        verbose_name_plural = "ArSecc"
 
+class cargo(models.Model):
+    cargo = models.CharField(max_length=200, verbose_name='Cargo', default='')
+    accesos = models.ManyToManyField(Acceso, through='CarAcc', blank=True,)
+
+    def __str__(self):
+        return self.cargo
+
+class persona(models.Model):
+    nombre = models.CharField(max_length=200, verbose_name='Nombre', default='')
+    apellido = models.CharField(max_length=200, verbose_name='Apellido', default='')
+    usr = models.CharField(max_length=10, verbose_name='Usuario', default='')
+    cargo = models.ForeignKey(cargo, on_delete=models.CASCADE)
+    fechaIngreso = models.DateField(verbose_name='Fecha de Ingreso')
+    fechaRetiro = models.DateField(verbose_name='Fecha de Retiro', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class CarAcc(models.Model):
+    cargo = models.ForeignKey(cargo, on_delete=models.CASCADE)
+    Acceso = models.ForeignKey(Acceso, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = "Secc_Acc"
 
  #   def nombre(self):
  #      return "{}".format(self.Nombre_de_Proyecto)
